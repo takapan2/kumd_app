7d73149ba8811fc3a46fecc3424ae475208d9f13
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-04-10 17:40:11 +0900

    displayのサーバーサイド修正

--- kumd-display-app/routes/display.js
+++ kumd-display-app/routes/display.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const USERDATA = require('../public/information/user_info').userInformation;
+const LIST = require('../public/information/list').LIST;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('display/index', { title: 'Express' });
+  let data = [[],[],[],[],[],[]];
+  for(var paint in USERDATA ){
+    data[parseInt( USERDATA[paint].grade)-1].push(USERDATA[paint]);
+  }
+  for(var i in data){
+    data[i].sort((a,b)=>{
+      if(a.size > b.size){
+        return 1;
+      }else{
+        return -1;
+      }
+    });
+  }
+  console.log(data);
+  res.render('display/index', { data: data, list:LIST });
 });
 
 module.exports = router;
