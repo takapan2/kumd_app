b2bcd97916da5bf6661b306c89e3c8b38e66483b
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-04-11 16:09:02 +0900

    スワイパー導入

--- kumd-display-app/public/javascripts/main.js
+++ kumd-display-app/public/javascripts/main.js
@@ -6,4 +6,15 @@ $(function() {
             'scrollTop': position
         },750);
     });
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
 });
