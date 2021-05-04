da1973c7fd51d7bcf11c9c0c48c7f346600f271d
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-05-04 20:19:52 +0900

    firestoreの使い方

--- kumd-display-app/app.js
+++ kumd-display-app/app.js
@@ -11,6 +11,53 @@ var displayRouter = require('./routes/display');
 
 var app = express();
 
+(async () => {
+  try {
+    const firebase = require('firebase')
+    const CONFIG = require('./public/information/config').config
+    firebase.initializeApp(CONFIG)
+    const db = firebase.firestore()
+
+    const userRef = db.collection('user').doc('GiPyiR86LfTUqGvilSUt')
+    const userDoc = await userRef.get()
+    if (userDoc.exists) {
+      console.log(userDoc.id)
+      console.log(userDoc.data())
+      console.log(userDoc.get('name'))
+      console.log(userDoc)
+    } else {
+      console.log('No such document!')
+    }
+    await db.app.delete()
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err)}`)
+  }
+})()
+
+
+// function CAAA(){
+// const firebase = require('firebase');
+// const config = {
+//       apiKey: "AIzaSyA4chVLLT13u2-zlfDlt7p5GXD_98DgY2Q",
+//       authDomain: "kumd-app.firebaseapp.com",
+//       databaseURL: "https://kumd-app-default-rtdb.firebaseio.com",
+//       projectId: "kumd-app",
+//       storageBucket: "kumd-app.appspot.com",
+//       messagingSenderId: "28972770999",
+//       appId: "1:28972770999:web:59ad63f788f272e4868f32",
+//       measurementId: "G-YPKJW425F5"
+// };
+// const CONFIG = require('./public/information/config').config;
+// firebase.initializeApp(CONFIG);
+// const db = firebase.firestore();
+// const userRef = db.collection('user').doc('GiPyiR86LfTUqGvilSUt')
+// const userDoc = userRef.get()
+// console.log(userDoc);
+// }
+
+// CAAA();
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
