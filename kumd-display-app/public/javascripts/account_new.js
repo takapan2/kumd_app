de8650d196247cb8490c294985cc0dca4377a1e6
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-05-20 23:26:16 +0900

    account_new than続きの構造からtry catchに変更

--- kumd-display-app/public/javascripts/account_new.js
+++ kumd-display-app/public/javascripts/account_new.js
@@ -1,16 +1,49 @@
+var file;
+var uid;
+var imgNum;
+var imgUid;
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const VALIDATION = {
+    FILE:{
+        VAL:"ファイルを選択してください。",
+        SIZE:"5MB以下のファイルを選択してください。",
+    },
+    GRADE:"回生を選択してください。",
+    SIZE:"サイズを選択してください。",
+    PENNAME:"ペンネームを入力してください。",
+    TITLE:"タイトルを入力してください。",
+    CAPTION:"説明文を入力してください。",
+}
+
+// 定数等
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         console.log("There's a user");
-        // console.log('uid',firebase.auth().currentUser.uid);
+        thereUser();
     } else {
         console.log("No user");
-        location.href='/login';
+        noUser();
     }
 });
+//
+async function thereUser(){
+    try {
+        uid = await firebase.auth().currentUser.uid;
+    } catch (err){
+        console.log(err);
+    }
+}
+async function noUser(){
+    location.href = "/login";
+}
 
-var file;
-var fileContent = document.getElementById('file');
-var fileButton = document.getElementById('submit-btn');
+
+const fileContent = document.getElementById('file');
+const submitButton = document.getElementById('submit-btn');
+const returnButton = document.getElementById('return');
 
 const fileErrMsg = document.querySelector('#file-err');
 const gradeErrMsg = document.querySelector('#grade-err');
@@ -19,140 +52,69 @@ const pennameErrMsg = document.querySelector('#penname-err');
 const titleErrMsg = document.querySelector('#title-err');
 const captionErrMsg = document.querySelector('#caption-err');
 
+//ファイルが変更された際の処理
 fileContent.addEventListener('change',function(e){
     file = e.target.files[0];
     console.log(file.size);
 });
 
-fileButton.addEventListener("click",()=>{
-    console.log("click fileButton");
+submitButton.addEventListener("click",()=>{
+    console.log("click submitButton");
+    //validationのメッセージを一度非表示にする。
     fileErrMsg.innerText = "";
     gradeErrMsg.innerText = "";
     sizeErrMsg.innerText = "";
     pennameErrMsg.innerText = "";
     titleErrMsg.innerText = "";
     captionErrMsg.innerText = "";
+    //各inputのvalueを取得。
     const gradeValue = document.getElementById('grade-select').value;
     const sizeValue = document.getElementById('size-select').value;
     const pennameValue = document.getElementById('penname').value;
     const titleValue = document.getElementById('title').value;
     const captionValue = document.getElementById('caption').value;
-    validation(file,gradeValue, sizeValue, pennameValue, titleValue, captionValue);
-    console.log("gradeValue",gradeValue);
-    console.log("sizeValue",sizeValue);
-    console.log("pennameValue",pennameValue);
-    console.log("titleValue",titleValue);
-    console.log("captionValue",captionValue);
-    const uid = firebase.auth().currentUser.uid;
-    var imgNum;
-    var usersData;
-    const db = firebase.firestore();
-    //既に画像が公開されているかをチェック
-    const userRef = db.collection('users').doc(uid);
-    userRef.get().then((doc)=>{
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-            usersData = doc.data();
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-        imgNum = usersData.imgNum+1;
-        usersData["img" + imgNum ] = uid +"-"+ imgNum;
-        usersData["imgNum"] = imgNum;
-        console.log("userData",usersData);
-        //ユーザーの保存(firestore)
-        db.collection("users").doc(uid).set(usersData)
-            .then(() => {
-                console.log("Users Data successfully written!");
-            })
-            .catch((error) => {
-                console.error("Error writing Users Data: ", error);
-        });
-        //イラストの保存(firestore)
-        db.collection("imgs").doc(uid +"-"+ imgNum ).set({
-            grade: gradeValue,
-            size: sizeValue,
-            penname: pennameValue,
-            title: titleValue,
-            caption: captionValue,
-        })
-        .then(() => {
-            console.log("Imgs Data successfully written!");
-        })
-        .catch((error) => {
-            console.error("Error writing Imgs Data: ", error);
-        });
-        //投票数やコメントの保管
-        db.collection("crients").doc(uid +"-"+ imgNum ).set({
-            accountNum: 0,
-            vote: 0,
-        })
-        .then(() => {
-            console.log("Crients Data successfully written!");
-        })
-        .catch((error) => {
-            console.error("Error writing Crients Data: ", error);
-        });
-        //イラストの保存(storage)
-        //イラスト一枚につき100kBほどに抑えたい。sizeとしては100000としたい。
-        new Compressor(file,{
-            quality:judgeQuality(file.size),
-            success(result) {
-            console.log(result);
-            var storageRef = firebase.storage().ref().child("imgs/"+ uid +"-"+ imgNum+".jpg");
-            storageRef.put(result);
-            },
-            error(err){
-                console.log('Compressor ERR',err);
-            }
-        });
-    })
-    .catch((err)=>{
-        console.log("Error getting Users Data:", err)
-    })
+    validation( file, gradeValue, sizeValue, pennameValue, titleValue, captionValue);
+    const imgsObject = {
+        grade: gradeValue,
+        size: sizeValue,
+        penname: pennameValue,
+        title: titleValue,
+        caption: captionValue,
+    }
+    const crientsObject = {
+        commentNum: 0,
+        vote: 0,
+    };
+    console.log("submitObject", imgsObject);
+    //ここにsubmitbtn
+    submitBtnFunc(uid, imgsObject, crientsObject, file);
 });
 
-const returnButton = document.getElementById('return');
 returnButton.addEventListener("click",()=>{
     if(window.confirm('変更内容は保存されませんが戻ってもよろしいでしょうか？')){
         location.href = '/login/account';
     }
 });
 
-// function judgeQuality(fileSize){
-//     console.log("judgeQuality");
-//     if(fileSize<500000){
-//         console.log(300000/fileSize);
-//         return 100000/fileSize;
-//     }else if(fileSize<1500000){
-//         console.log(500000/fileSize);
-//         return 150000/fileSize;
-//     }else if(fileSize<3000000){
-//         console.log(750000/fileSize);
-//         return 250000/fileSize;
-//     }else if(fileSize<10000000){
-//         console.log(1000000/fileSize);
-//         return 1000000/fileSize;
-//     }else{
-//         console.log(10000000/fileSize);
-//         return 10000000/fileSize;
-//     }
-// }
-function judgeQuality(fileSize){
-    const M = 1024;
-    if(fileSize<M*0.5){
-        console.log(0.8);
-        return 0.8;
-    }else if(fileSize<M*M){
-        console.log(0.6);
-        return 0.6;
-    }else if(fileSize<2*M*M){
-        console.log(0.4);
-        return 0.4;
-    }else{
-        console.log(0.2);
-        return 0.2;
+async function submitBtnFunc(uid, imgsObject, crientsObject, file ){
+    try{
+        const userData = await getStoreData('users',uid);
+        imgNum = userData.imgNum + 1;
+        imgUid = `${uid}-${imgNum}`;
+        imgChild = `imgs/${imgUid}.jpeg`;
+        const UserObject = {
+            id: imgUid,
+            imgNum: imgNum,
+            [`img${imgNum}`]:imgUid,
+        };
+        await fileCompressAndSave(file,imgChild);
+        await dataUpdate(UserObject,'users',uid);
+        await setStoreData(imgsObject, "imgs", imgUid);
+        await setStoreData(crientsObject, "crients", imgUid);
+        console.log("save finish!!");
+        location.href = "/login/account";
+    }catch(err){
+        console.log("submitBtnFunc err",err);
     }
 }
 
@@ -161,8 +123,7 @@ function validation(file,gradeValue, sizeValue, pennameValue, titleValue, captio
     if(!file){
         fileErrMsg.innerText = VALIDATION.FILE.VAL;
         judge++;
-    }
-    if(file>5*1024*1024){
+    }else if(file.size>5*1024*1024){
         fileErrMsg.innerText = VALIDATION.FILE.SIZE;
         judge++;
     }
@@ -187,4 +148,75 @@ function validation(file,gradeValue, sizeValue, pennameValue, titleValue, captio
         judge++;
     }
     if(judge!=0)throw Error("Validation!!");
+}
+
+//共通関数ーーーーーーーーーーーーーーーーーーーー
+
+//ファイル圧縮しを保存しするところまで行う。
+function fileCompressAndSave(file,child){
+    return new Compressor(file,{
+        quality:judgeQuality(file.size),
+        success(result) {
+            console.log(result);
+            saveStorageData(result,child);
+        },
+        error(err){
+            console.log('Compressor ERR',err);
+        }
+    });
+}
+//firestorageに画像を保存する。
+function saveStorageData(file,child){
+    var storageRef = firebase.storage().ref().child(child);
+    storageRef.put(file);
+}
+
+//ファイルのサイズによって圧縮率を変更する。
+function judgeQuality(fileSize){
+    const M = 1024;
+    if(fileSize<M*0.5){
+        console.log(0.8);
+        return 0.8;
+    }else if(fileSize<M*M){
+        console.log(0.6);
+        return 0.6;
+    }else if(fileSize<2*M*M){
+        console.log(0.4);
+        return 0.4;
+    }else{
+        console.log(0.2);
+        return 0.2;
+    }
+}
+
+async function getStoreData(collection,doc){
+    const storeRef = db.collection(collection).doc(doc);
+    const storeData = await storeRef.get();
+    if(await storeData.exists){
+        return storeData.data();
+    }else{
+        throw new Error(`No such document! collection= ${collection}, doc= ${doc}`);
+    }
+}
+
+//firestoreからデータを取得する。
+async function getStoreData(collection,doc){
+    const storeRef = db.collection(collection).doc(doc);
+    const storeData = await storeRef.get();
+    if(await storeData.exists){
+        return storeData.data();
+    }else{
+        throw new Error(`No such document! collection= ${collection}, doc= ${doc}`);
+    }
+}
+
+function dataUpdate(object,collection,document){
+    const storeRef = db.collection(collection).doc(document);
+    return storeRef.update(object);
+}
+
+//firestoreにデータを保存する
+function setStoreData(object, collection, doc){
+    const storeRef = db.collection(collection).doc(doc);
+    return storeRef.set(object);
 }
