fca7d029a2f5a7cde741eaf9a22e21a94915d82c
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-05-24 04:34:41 +0900

    不要なものの削除2

--- kumd-display-app/routes/display.js
+++ kumd-display-app/routes/display.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const DEMODATA = require('../public/information/demo_data').demodata;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
