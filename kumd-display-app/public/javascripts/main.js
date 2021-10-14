d4dadcb155b224a0b26d66ea2948fdb1cc8b29c4
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-10-15 03:39:06 +0900

    caption周りのデザイン修正

--- kumd-display-app/public/javascripts/main.js
+++ kumd-display-app/public/javascripts/main.js
@@ -7,16 +7,23 @@ $(function() {
         },750);
     });
 
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'progressbar',
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-    });
+    // var swiper = new Swiper('.swiper-container', {
+    //     pagination: {
+    //       el: '.swiper-pagination',
+    //       type: 'progressbar',
+    //     },
+    //     navigation: {
+    //       nextEl: '.swiper-button-next',
+    //       prevEl: '.swiper-button-prev',
+    //     },
+    // });
+    var swiper = new Swiper(".swiper-container", {
+      observer: true,
+      observeParents: true,
+      pagination: {
+          el: ".swiper-pagination",
+      },
+  });
 });
 
 //ヘッダー部分の挙動
