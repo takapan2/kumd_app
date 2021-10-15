80a44ebd915bca2d9fe04f5dee27961d08257c82
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-10-15 10:56:16 +0900

    captionのswiperのクリック対応

--- kumd-display-app/public/javascripts/main.js
+++ kumd-display-app/public/javascripts/main.js
@@ -7,23 +7,15 @@ $(function() {
         },750);
     });
 
-    // var swiper = new Swiper('.swiper-container', {
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       type: 'progressbar',
-    //     },
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-    // });
     var swiper = new Swiper(".swiper-container", {
-      observer: true,
-      observeParents: true,
       pagination: {
-          el: ".swiper-pagination",
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>';
+        },
       },
-  });
+    });
 });
 
 //ヘッダー部分の挙動
